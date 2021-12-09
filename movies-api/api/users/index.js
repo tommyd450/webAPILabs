1611c8deae08d43e74a5188c2aa922f1ce586732
tommyd450 <tommyd811@gmail.com>
2021-12-09 15:00:52 +0000

    Topic 10 - Object Referencing

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import movieModel from '../movies/movieModel';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -37,28 +38,21 @@ router.post('/',asyncHandler( async (req, res, next) => {
       }
   }));
 
-router.post('/:id/favourites', async (req, res) => {
-    const newFavourite = req.body;
-    if (newFavourite && newFavourite.id) {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            user.favourites.push(newFavourite);
-            user.save();
-            res.status(201).json({ code: 201, msg: "Added Favourite" });
-        } else {
-            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
-        }
-    }
-});
+  router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+  }));
 
-router.get('/:id/favourites', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        res.status(200).json(user.favourites);
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
-    }
-});
+  router.get('/:userName/favourites', asyncHandler( async (req, res) => {
+    const userName = req.params.userName;
+    const user = await User.findByUserName(userName).populate('favourites');
+    res.status(200).json(user.favourites);
+  }));
 
  // Update a user
  router.put('/:id', async (req, res) => {
