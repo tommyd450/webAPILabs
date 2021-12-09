638582f31f880dfd0b6a5762c64e2d39a7c44014
tommyd450 <tommyd811@gmail.com>
2021-12-09 13:20:01 +0000

    Topic 10 - JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -12,28 +13,29 @@ router.get('/', async (req, res) => {
 
 // register(Create)/Authenticate User
 // register
-router.post('/', asyncHandler(async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
+router.post('/',asyncHandler( async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      res.status(401).json({success: false, msg: 'Please pass username and password.'});
+      return next();
+    }
+    if (req.query.action === 'register') {
+      await User.create(req.body);
+      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    } else {
+      const user = await User.findByUserName(req.body.username);
+        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            // if user is found and password matches, create a token
+            const token = jwt.sign(user.username, process.env.SECRET);
+            // return the information including token as JSON
+            res.status(200).json({success: true, token: 'BEARER ' + token});
+          } else {
+            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+          }
         });
-    }
-    else {  //NEW CODE!!!
-        const user = await User.findByUserName(req.body.username);
-        if (user.comparePassword(req.body.password)) {
-            req.session.user = req.body.username;
-            req.session.authenticated = true;
-            res.status(200).json({
-                success: true,
-                token: "temporary-token"
-              });
-        } else {
-            res.status(401).json('authentication failed');
-        }
-    }
-}));
+      }
+  }));
 
 router.post('/:id/favourites', async (req, res) => {
     const newFavourite = req.body;
